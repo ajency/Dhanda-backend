f0059dc0ba8b493572d4bfab08e90b6dc7d2ec71
sharangsamonkar <sharang@ajency.in>
2020-10-08 17:53:54 +0530

    added generateOtp method

---
+++ services/v1/OtpService.js
@@ -0,0 +1,18 @@
+const logger = require("simple-node-logger").createSimpleLogger({ timestampFormat:'YYYY-MM-DD HH:mm:ss.SSS' });
+const defaults = require("../defaults");
+
+module.exports = class OtpService {
+    async generateOtp() {
+        await logger.info("Generate OTP");
+
+        /** Fetch the otp length */
+        let otpLength = defaults.getValue("otp").length;
+
+        /** Generate a random otp */
+        let add = 1, max = 12 - add;
+        max = Math.pow(10, otpLength + add);
+        let min = max / 10;
+        let number = Math.floor( Math.random() * (max - min + 1) ) + min;
+        return ("" + number).substring(add);
+    }
+}
