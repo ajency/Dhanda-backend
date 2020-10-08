4c01a8b5f7c92e90cce957a89610d6f02f9fe5e8
sharangsamonkar <sharang@ajency.in>
2020-10-08 15:45:42 +0530

    change the timestamps to snake case and remove extra migration

--- migrations/20200523111850-create-auth-token.js
+++ migrations/20200523111850-create-auth-token.js
@@ -23,11 +23,11 @@ module.exports = {
       valid_till: {
         type: Sequelize.DATE
       },
-      createdAt: {
+      created_at: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
         type: Sequelize.DATE
       }
