58373a58282ad3d4e331f400c5023ea689b1fe38
sharangsamonkar <sharang@ajency.in>
2020-10-14 21:28:50 +0530

    added staff model

---
+++ migrations/20201014155657-create-staff.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('staffs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      business_id: {
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      country_code: {
+        type: Sequelize.INTEGER
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      salary_type_txid: {
+        type: Sequelize.INTEGER
+      },
+      salary: {
+        type: Sequelize.INTEGER
+      },
+      cycle_start_day: {
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('staffs');
+  }
+};
