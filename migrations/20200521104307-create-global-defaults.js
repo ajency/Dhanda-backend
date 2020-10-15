8560fc72ad365b07deb250ad8caddac0b6cd0dce
sharangsamonkar <sharang@ajency.in>
2020-10-15 15:56:45 +0530

    changed global default meta column from text to json

--- migrations/20200521104307-create-global-defaults.js
+++ migrations/20200521104307-create-global-defaults.js
@@ -15,7 +15,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       meta_data: {
-        type: Sequelize.TEXT
+        type: Sequelize.JSON
       },
       created_at: {
         allowNull: false,
