2486400be9910949d7d43da3097b114b714b33c0
sharangsamonkar <sharang@ajency.in>
2020-11-12 16:21:11 +0530

    added failed jobs table

---
+++ models/failed_jobs.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class failed_jobs extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  failed_jobs.init({
+    queue_name: DataTypes.STRING,
+    payload: DataTypes.JSON
+  }, {
+    sequelize,
+    modelName: 'failed_jobs',
+    underscored: true,
+  });
+  return failed_jobs;
+};
