c5ef1c343ec39f6cb43ced578943aace91382c3f
Sharang Sinai Amonkar <sharang@ajency.in>
2020-11-22 16:53:08 +0530

    minor change

--- api/v1/index.js
+++ api/v1/index.js
@@ -52,6 +52,6 @@ router.get("/cron/populateDailyAttendanceAndPayroll", cronController.populateDai
 
 /** Internal */
 router.get("/addRule", defaultController.addRule);
-router.get("calculatePayrollForBusiness", attendanceController.calculatePayrollForBusiness);
+router.get("/calculatePayrollForBusiness", attendanceController.calculatePayrollForBusiness);
 
 module.exports = router;
