b0664ee2f1beb2c1ecf04e7de6bcbf20fc839c87
Sharang Sinai Amonkar <sharang@ajency.in>
2020-12-06 12:01:29 +0530

    corrected convert mins to hours string method

--- services/HelperService.js
+++ services/HelperService.js
@@ -112,8 +112,8 @@ module.exports = class HelperService {
     }
 
     convertMinutesToHoursString(minutes) {
-        let hours = minutes % 60;
-        let min = Math.floor(minutes / 60);
+        let hours = Math.floor(minutes / 60);
+        let min = minutes % 60;
         return ("00" + hours).slice(-2) + ":" + ("00" + min).slice(-2);
     }
 
